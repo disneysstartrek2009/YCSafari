bebddc8bf2979b0918af9920a7af7d5ede20b78d
Ethan <ethan021@me.com>
2016-01-18 01:46:00 -0800

    Initial Commit

---
+++ mySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mySafari
+//
+//  Created by Ethan Thomas on 1/18/16.
+//  Copyright © 2016 Ethan Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mySafari/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mySafari
+//
+//  Created by Ethan Thomas on 1/18/16.
+//  Copyright © 2016 Ethan Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
